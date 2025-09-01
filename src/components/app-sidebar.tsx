b1fd6ec3e02e0e3afb1419e49615ae31185c9492
codequeen-11 <ayumeyra11@gmail.com>
2025-08-31 21:39:10 -0900

    some changes

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -23,7 +23,11 @@ interface User {
   id: string
   name?: string
   email: string
-  image?: string
+  // image?: string
+    emailVerified: boolean
+  createdAt: Date
+  updatedAt: Date
+  image?: string | null
 }
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
